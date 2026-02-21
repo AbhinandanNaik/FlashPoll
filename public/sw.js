6a31d0eff9be338abcb36101d0c0097d3323097b
abhinandan <abhinandannaik1717@gmail.com>
2026-02-21 14:49:55 +0530

    Step 19 & 20: CI/CD Pipeline & Production Polish

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'flashpoll-v1';
+const ASSETS_TO_CACHE = ['/', '/css/variables.css', '/css/style.css', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
